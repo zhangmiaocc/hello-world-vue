49729242d3842103372f84b42f8f38b3c5e39bb4
zm <479326215@qq.com>
2018-12-13 10:28:49 +0800

    no message

---
+++ src/views/About.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="about">
+    <h1 @click="clickHandler">This is an about page</h1>
+
+    <div>{{ message }}</div>
+    <div>{{ message2 }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  // 接受父组件参数
+  props: {
+    propName: {
+      type: Number,
+      default: 0
+    },
+    propName2: ''
+  },
+  components:[],
+  data() {
+    return {
+      message: 'Hello Vue!',
+      message2: 'Hello Vue!'
+    }
+  },
+  // 计算属性，依赖于data
+  computed: {
+    name() {
+      return this.message+' '+this.message2
+    },
+    name2:{
+      get:function(){
+        return this.message+' '+this.message2
+      },
+      set: function(val){
+        this.message = val
+      }
+    }
+  },
+  watch: {
+    message(newValue, oldValue) { },
+    message2(val){},
+    name(val){}
+  },
+  created(){},
+  mounted(){},
+  methods: {
+    clickHandler() {
+      this.message = 'xxxxxx'
+    }
+  },
+};
+</script>
+
